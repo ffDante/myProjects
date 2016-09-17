f9936cbefed182055b77a833f457d55a12dc260e
ffDante <ffortunatoff@gmail.com>
2016-09-16 23:54:17 -0400

    Initial Docks project commit

---
+++ Docks/src/com/vitalii/docks/main/HeavyContainer.java
@@ -0,0 +1,9 @@
+package com.vitalii.docks.main;
+
+public class HeavyContainer extends Container{
+
+	public HeavyContainer(boolean toxic, boolean explosive) {
+		super(toxic, explosive);
+	}
+
+}
---
+++ Docks/src/com/vitalii/docks/main/RefrigeratedContainer.java
@@ -0,0 +1,9 @@
+package com.vitalii.docks.main;
+
+public class RefrigeratedContainer extends HeavyContainer {
+
+	public RefrigeratedContainer(boolean toxic, boolean explosive) {
+		super(toxic, explosive);
+	}
+
+}
